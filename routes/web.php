c551fca203d5ca2be92e92b3506955a199d56c5d
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-05-21 15:00:21 +0530

    update web file

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,21 @@ use App\Http\Controllers\PagesController;
 //     return view('home');
 // });
 
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache cleared successfully!";
+});
+
+
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created successfully!';
+});
+
 Route::get('/send-sms', [SMSController::class, 'sendTestSMS']);
 
 // In routes/web.php
