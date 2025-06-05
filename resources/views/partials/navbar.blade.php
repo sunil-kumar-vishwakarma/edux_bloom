0f48c21149b3ecfaa744ae3883f6073ea7b20e8d
krishh2001 <161043749+krishh2001@users.noreply.github.com>
2025-06-05 15:15:05 +0530

    new

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,46 +8,44 @@
         </button>
     </div>
     <div class="navbar-right">
-        <div class="profile-icon">
-            @auth
-                @if (Auth::user()->profile && Auth::user()->profile->profile_photo)
-                    <img src="{{ asset('storage/' . Auth::user()->profile->profile_photo) }}" alt="Profile"
-                        class="profile-img">
-                @else
-                    <img src="{{ asset('image/admin logo.png') }}" alt="Default Profile" class="profile-img">
-                @endif
-            @else
-                <img src="{{ asset('image/admin logo.png') }}" alt="Default Profile" class="profile-img">
-            @endauth
-        </div>
+     <div class="profile-icon">
+    @auth
+        @if(Auth::user()->profile && Auth::user()->profile->profile_photo)
+            <img src="{{ asset('public/storage/' . Auth::user()->profile->profile_photo) }}" alt="Profile" class="profile-img">
+        @else
+            <img src="{{ asset('public/image/admin logo.png') }}" alt="Default Profile" class="profile-img">
+        @endif
+    @else
+        <img src="{{ asset('public/image/admin logo.png') }}" alt="Default Profile" class="profile-img">
+    @endauth
+</div>
 
-        <span class="navbar-user">
-            @auth
-                {{ Auth::user()->profile->name ?? Auth::user()->name }}
-            @else
-                Guest
-            @endauth
-        </span>
+<span class="navbar-user">
+    @auth
+        {{ Auth::user()->profile->name ?? Auth::user()->name }}
+    @else
+        Guest
+    @endauth
+</span>
 
 
 
         <div class="dropdown-menu">
             {{-- <a href="{{ route('profile') }}" class="dropdown-item">Profile</a> --}}
 
-            @if (Auth::check())
-                <!-- User is authenticated, so you can access $user->id -->
-                <a href="{{ route('profile', Auth::user()->id) }}" class="dropdown-item">View Profile</a>
-            @else
-                <!-- If the user is not authenticated -->
-                <a href="{{ route('login') }}">Login</a>
-            @endif
+              @if(Auth::check())
+              <!-- User is authenticated, so you can access $user->id -->
+              <a href="{{ route('profile', Auth::user()->id) }}" class="dropdown-item">View Profile</a>
+          @else
+              <!-- If the user is not authenticated -->
+              <a href="{{ route('login') }}">Login</a>
+          @endif
 
             <!-- Logout Functionality -->
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-            <a href="#" class="dropdown-item"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+            <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
         </div>
     </div>
 </header>
