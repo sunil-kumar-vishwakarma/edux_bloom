3f7db9cd92a3ee57ada2db9f83377999a065f853
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-06-07 13:07:25 +0530

    gddff change images permission

--- resources/views/frontent_partials/navbar.blade.php
+++ resources/views/frontent_partials/navbar.blade.php
@@ -204,9 +204,9 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link
+<!-- <link
     href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
-    rel="stylesheet">
+    rel="stylesheet"> -->
 <script>
     document.querySelector('.navbar-toggler').addEventListener('click', function() {
         const navbarCollapse = document.querySelector('.navbar-collapse');
